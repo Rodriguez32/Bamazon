8b4557efed8407663b3b38d8dac6a9b7d427fd86
Azalea <Rodriguez32.ilda0122@gmail.com>
2018-08-23 20:34:46 -0500

    added js and schema

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,26 +17,79 @@ var connection = mysql.createConnection({
 });
 
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    
-    // run the start function after a connection is made
-    start();
-    // connection.end();
-  });
+connection.connect(function (err) {
+  if (err) throw err;
+  // console.log("connected as id " + connection.threadId);
 
-  function start(){
-    connection.query("SELECT * FROM products", function(err, results) {
-      if (err) throw err;
-    
-      inquirer
-    .prompt([
-      {
-      name: "item",
-      type: "input",
-      message: "What is the item_id of the product you would like to buy?"
-      },
-    ])
+  // run the start function after a connection is made
+  start();
+  // connection.end();
+});
+
+function start() {
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+
+    console.log("******************************************* Welcome to Bamazon  *********************************************************")
+    console.log("-------------------------------------------------------------------------------------------------------------------------")
+
+    // Inspired by kindergarten list of supplies
+    for (var i = 0; i < res.length; i++) {
+      console.log("ID: " + res[i].item_id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "Qty: " + res[i].stock_quantity)
+    }
+
+    console.log("-------------------------------------------------------------------------------------------------------------------------")
+
+    inquirer
+      .prompt([
+        {
+          name: "item",
+          type: "id",
+          message: "What is the ID of the product you would like to buy?",
+          validate: function (value) {
+            if (isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+        },
+        {
+          type: "input",
+          name: "qty",
+          message: "How many units would you like to buy?",
+          validate: function (value) {
+            if (isNaN(value)) {
+              return false;
+            } else {
+              return true;
+            }
+          }
+        }
+      ]).then(function (ans) {
+        // console.log(ans);
+        // console.log("This is response" + res);
+        var whatToBuy = (ans.item) - 1;
+        //  console.log(res[whatToBuy].price);
+        var unitsToBuy = parseInt(ans.qty);
+        var grandTotal = parseFloat(((res[whatToBuy].price) * unitsToBuy).toFixed(2));
+        // console.log(grandTotal);
+
+        //check if quantity is sufficient
+        if (res[whatToBuy].stock_quantity >= unitsToBuy) {
+          //after purchase, updates quantity in products
+          connection.query("UPDATE products SET ? WHERE ?", [
+            { stock_quantity: (res[whatToBuy].stock_quantity - unitsToBuy) },
+            { item_id: ans.id }
+          ], function (err, result) {
+            if (err) throw (err);
+            console.log("Success! Your total is $" + grandTotal.toFixed(2) + ". Your item(s) will be shipped to you in 3-5 business days.")
+          }
+          )
+        }
+
+
+      });
   }
-)}
+  )
+}
--- schema.sql
+++ schema.sql
@@ -10,3 +10,20 @@ price decimal(10,2) not null,
 stock_quantity int(10) not null,
 primary key(item_id)
 );
+
+select * from products;
+
+
+
+insert into products(product_name,department_name,price,stock_quantity)
+values ("Fire 7 Kids Edition Tablet","Electronics",79.99,150),
+	("Badger Suncscreen","Health",13.56,25),
+    ("PlayDoh 10-pack case","Arts&Crafts",7.99,25),
+    ("Child-Safe Scissors Activity book","Arts&Crafts",4.99,12),
+    ("Kleenex wet wipes","Household",14.72,10),
+    ("Crayola 24 count box","Toys&Games",5.60,100),
+    ("Mead Primary Journal","Toys&Games",6.59,50),
+    ("First day of school photo board","Toys&Games",21.95,50),
+    ("Rubber Rain boots for boys","Kids",13.90,50),
+    ("Kidorable kids space umbrella","Kids",18.00,25);
+    
